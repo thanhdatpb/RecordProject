c6cedeab130fb7c090398486807c6c9d330c69f1
Thanh Dat <tthanhdat.pb@gmail.com>
2024-03-07 00:39:39 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,18 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+        Student pojoStudent = new Student("S923006", "Ann",
+                "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill",
+                "05/11/1985", "Java Masterclass");
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is talking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is talking " + recordStudent.classList());
+
     }
 }
