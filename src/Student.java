5b09666e87112f923c25709ab9d2b2e77425079a
Thanh Dat <tthanhdat.pb@gmail.com>
2024-03-06 20:30:19 +0700

    update code

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,8 @@
 public class Student {
-    private final String id;
-    private final String name;
-    private final String dateOfBirth;
-    private final String classList;
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private String classList;
 
     public Student(String id, String name, String dateOfBirth, String classList) {
         this.id = id;
@@ -21,4 +21,31 @@ public class Student {
                 '}';
     }
 
+    public String getId() {
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getClassList() {
+        return classList;
+    }
+    public void setClassList(String classList){
+        this.classList = classList;
+    }
 }
